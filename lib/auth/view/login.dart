7e4e0ac4a36866a2698def1bac082c17986eedc5
admin <industrialautomationhub028@gmail.com>
2023-04-26 01:06:36 +0530

    ph
    Signed-off-by: admin <industrialautomationhub028@gmail.com>

--- lib/auth/view/login.dart
+++ lib/auth/view/login.dart
@@ -1,6 +1,7 @@
 import 'package:automation_hub/auth/controller/auth_controller.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../utils/colors.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -10,28 +11,50 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool isAgreeTc = false;
+  double height = 0.0;
+  double width = 0.0;
   @override
   Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          //const Padding(
-          // padding: EdgeInsets.only(left: 30),
-          //child: Text(
-          //"Industrial Automation",
-          //style: TextStyle(
-          //fontSize: 35,
-          //),
-          //),
-          //),
-          const SizedBox(height: 47),
+          Image.asset("assets/app_icon_splash/icon_one.jpg", fit: BoxFit.fill),
+          Row(
+            children: [
+              Checkbox(
+                  value: isAgreeTc,
+                  activeColor: ThemeColors.checkBoxActiveColor,
+                  onChanged: (value) {
+                    setState(() {
+                      isAgreeTc = !isAgreeTc;
+                    });
+                  }),
+              Container(
+                width: width / 1.3,
+                child: const Text(
+                  "I agree the Terms&Condition of the application",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 50),
           ElevatedButton(
-              onPressed: () {
-                signup(context);
-              },
+              style: ElevatedButton.styleFrom(
+                maximumSize: Size(width / 1.5, height * 0.06),
+              ),
+              onPressed: isAgreeTc == true
+                  ? () {
+                      signup(context);
+                    }
+                  : null,
               child: const Text("Login with Google"))
         ],
       ),
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ThemeColors {
   static Color Primarycolor = const Color(0xffFEBE03);
+  static Color checkBoxActiveColor = const Color(0xfff5c855);
   static Color unselectediconcolor = Colors.grey;
 
 }
